2960179777a17d8729f1a0f1e83ba7be75fe3ee6
irimina <irimina@gmail.com>
2018-08-05 12:14:44 -0400

    Create pokeball.py

---
+++ pokeball.py
@@ -0,0 +1,16 @@
+
+'''
+
+1) Write a program called Pokeball that displays the following output:
+
+   @@@@@@@@@@
+  @@@@@@@@@@@@
+ @@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@
+|||||[““““]|||||
+|||||[....]|||||
+OOOOOOOOOOOOOOOO
+ OOOOOOOOOOOOOO
+  OOOOOOOOOOOO
+   OOOOOOOOOO
+'''
